314d833f0c5bd92cb5aceb2b18773e0cc3afc519
ryuenjin <54440789+ryuenjin@users.noreply.github.com>
2020-06-25 19:41:36 -0300

    InfinityRun
    Infinity run, com menus, botões

---
+++ Infinity Run 3D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Transform player;
+    private Vector3 offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offSet = transform.position - player.position;
+
+
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, player.position.z + offSet.z);
+        transform.position = newPosition; 
+
+        
+    }
+}
